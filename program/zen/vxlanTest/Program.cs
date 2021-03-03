83a4ca57a90944429ba70f471f820db2b3d1b0da
椎葉瑠星 <siiba@sbnet.local>
2021-03-04 06:19:44 +0900

    add

---
+++ program/zen/vxlanTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ZenLib;
+using static ZenLib.Language;
+using ZenLib.Tests;
+
+namespace main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testNet = new VxlanNetworkTest();
+        }
+    }
+}
